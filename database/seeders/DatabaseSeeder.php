10f969f8837525ebd8ba94908c687eeaca3c15ee
Your Name <nurgozel009@gmail.com>
2023-10-29 22:54:25 +0500

    1.3.0

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,11 +12,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call([
+            FabricSeeder::class,
+            CategorySeeder::class,
+        ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\Fabric::factory(85)->create();
     }
 }
